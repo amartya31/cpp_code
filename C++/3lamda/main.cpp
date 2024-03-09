3d6ce415175b2a40a3fb379869195e38d7b38660
Amartya <ghosh.amartya31@gmail.com>
2024-03-09 18:18:53 +0530

    Added lamda

--- C++/3lamda/main.cpp
+++ C++/3lamda/main.cpp
@@ -18,9 +18,63 @@ int main(){
     }(5); // -> data passed 
 
     // lamda to add 2 numbers 
-    auto add = [](int a,int b){
+    auto add = [](int a,int b)->int{
         return (a+b);
-    }
+    };
+
+    int sum = add(2,3);
+
+    cout << "Sum Value = " << sum << endl;
+    
+    // Lamda function by value 
+
+    auto mul = [](int a)->int{
+        return a*2;
+    };
+    sum = mul(sum);
+    cout << "Sum Value = " << sum << endl;
+
+    // Lamda by reference 
+
+    auto diff = [](int &n){
+        n = n/2;
+    };
+
+    diff(sum);
+    cout << "Refference Vallue = " << sum  << endl;
+
+    auto mulR = [](int *n){
+        *n = (*n) * 2;
+    };
+
+    mulR(&sum);
+    cout << "Refference Vallue = " << sum  << endl;
+
+    // Capture [] in lamda 
+    int test = 5;
+    // By value 
+    auto showCapV = [test](){
+        int test1 =  test + 1; // note in value is constant 
+        cout << "Test by Value " << test1 << endl;
+    };
+    showCapV();
+    cout <<"Test value = " << test << endl;
+    // By reference 
+    auto showCapR = [&test](){
+        test =  test + 1; // note in value is constant 
+        cout << "Test by Reference " << test << endl;
+    };
+    showCapR();
+    cout <<"Test value = " << test << endl;
+    auto showSum = [&](){
+        sum = sum +1;
+        cout << "Sum by refernce  " << sum << endl;
+    };
+    showSum();
+    cout <<"Test val = " << test  << "  Sum = " << sum << endl;
+
+
+
 
     return(0);
 }
